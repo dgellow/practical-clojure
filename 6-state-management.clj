8e38a0ca89e2e03db700e55a009878c74e04607f
Sam E <georgesleyetidoux@gmail.com>
2013-05-24 20:27:04 +0200

    Adding definitions for coordinated, independent, synchronous and asynchronous concepts

--- 6-state-management.clj
+++ 6-state-management.clj
@@ -11,3 +11,15 @@
 ;; - atoms manage synchronous, independent state
 ;; - (vars ? Dunno, need some search)
 
+;; Coordinated updates have to manage the states of several interdependent identities
+;; to ensure that they are all updated at the same time and that none are left out.
+;;
+;; Independent identities stand on their own and an have their state updated without concern for other identities.
+
+;; Sychronous updates to the values identities occur immediately, in the same thread from which they are invoked.
+;; The execution of the code does not continue until the update has taken place.
+;;
+;; Asynchronous updates do not occur immediately, but at some unspecified point in the future, usually in another
+;; thread. The code execution continues immediately from the point at which the update was invoked, without waiting
+;; for it to complete.
+
